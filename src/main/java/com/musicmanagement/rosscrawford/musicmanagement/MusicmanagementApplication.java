b6c09294d3c46680a8367417c418dca720947211
Ross Crawford <ross.crawford.2013@uni.strath.ac.uk>
2020-11-24 12:57:18 +0000

    Initial commit.

---
+++ src/main/java/com/musicmanagement/rosscrawford/musicmanagement/MusicmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.musicmanagement.rosscrawford.musicmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musicmanagement/rosscrawford/musicmanagement/MusicmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicmanagement.rosscrawford.musicmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
