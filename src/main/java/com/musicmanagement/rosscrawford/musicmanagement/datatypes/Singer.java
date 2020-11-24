8d32b54672956de7814ae2867bd7f04d5dd0df03
Ross Crawford <ross.crawford.2013@uni.strath.ac.uk>
2020-11-24 13:50:12 +0000

    Created inital Data Types.
    Created the initial datatypes for the system, and established the tables and columns for MySQL database.

---
+++ src/main/java/com/musicmanagement/rosscrawford/musicmanagement/datatypes/Singer.java
@@ -0,0 +1,123 @@
+package com.musicmanagement.rosscrawford.musicmanagement.datatypes;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.persistence.Id;
+
+@Entity
+@Table(name = "singer", uniqueConstraints = @UniqueConstraint(columnNames = "name"))
+public class Singer implements Manageable{
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
+@Column(name = "name")
+private String name;
+
+@Column(name = "sex")
+private String sex;
+
+// Update this to object
+@Column(name = "company")
+private String company;
+
+@Column(name = "dob")
+private Date dob;
+
+/**
+     * Primary constructor.
+     *
+     * @param name the singer's name.
+     * @param company the company that manages singer.
+     */
+    public Singer(String name, String company) {
+        this.name = name;
+        this.company = company;
+    }
+
+    /**
+     * Secondary Constrcutor
+     *
+     * @param name the singer's name.
+     * @param company the company that manages the singer.
+     * @param dob the singer's date of birth.
+     * @param sex the singer's sex..
+     */
+    public Singer(String name, String company, Date dob, String sex) {
+        this.name = name;
+        this.company = company;
+        this.dob = dob;
+        this.sex = sex;
+    }
+
+    /**
+     * @return the singer's name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the singer's name.
+     * @return this.
+     */
+    public Singer setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    /**
+     * @return the company that manages the singer.
+     */
+    public String getCompany() {
+        return company;
+    }
+
+    /**
+     * @param company the company that manages the singer.
+     * @return this.
+     */
+    public Singer setCompany(String company) {
+        this.company = company;
+        return this;
+    }
+
+    /**
+     * @return the singer's date of birth.
+     */
+    public Date getDob() {
+        return dob;
+    }
+
+    /**
+     * @param dob the singer's date of birth.
+     * @return this.
+     */
+    public Singer setDob(Date dob) {
+        this.dob = dob;
+        return this;
+    }
+
+    /**
+     * @return the singer's sex.
+     */
+    public String getSex() {
+        return sex;
+    }
+
+    /**
+     * @param sex the singer's sex.
+     * @return this.
+     */
+    public Singer setSex(String sex) {
+        this.sex = sex;
+        return this;
+    }
+}
---
+++ src/main/java/com/musicmanagement/rosscrawford/musicmanagement/datatypes/User.java
@@ -0,0 +1,70 @@
+package com.musicmanagement.rosscrawford.musicmanagement.datatypes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.persistence.Id;
+
+
+@Entity 
+@Table(name = "user", uniqueConstraints = @UniqueConstraint(columnNames = "username"))
+/**
+ * Data Structure to represent a User of the system.
+ */
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    /**
+     * Primary constructor.
+     *
+     * @param username the user's username
+     */
+
+    public User(String username) { this.username = username;}
+
+
+    /**
+     * @return the user's username.
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the user's username.
+     * @return this.
+     */
+    public User setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    /**
+     * @return the user's password.
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the user's password.
+     * @return this.
+     */
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+}
