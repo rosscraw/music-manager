bddc84d4fb8c1cb6a3902e9cbd8dfec67c984c14
Ross Crawford <ross.crawford.2013@uni.strath.ac.uk>
2020-11-27 02:28:16 +0000

    Created pages for each data type.
    Created pages containing a table for each data type. Added navigation bar.

--- src/main/java/com/musicmanagement/SecurityConfig.java
+++ src/main/java/com/musicmanagement/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-      http.authorizeRequests()
+      http.httpBasic().and().authorizeRequests()
           .anyRequest().authenticated()
           .and()
           .formLogin().permitAll()
